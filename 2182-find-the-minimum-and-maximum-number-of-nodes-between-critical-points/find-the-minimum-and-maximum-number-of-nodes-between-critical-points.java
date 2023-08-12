bf77d7ca0130eb7632a72e4745c542805449439f
Pratishank Shukla <95025713+pratishank05@users.noreply.github.com>
2023-08-12 18:03:25 +0530

    Time: 5 ms (86.76%) | Memory: 69 MB (37.98%) - LeetSync

--- 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
@@ -10,39 +10,33 @@
  */
 class Solution {
     public int[] nodesBetweenCriticalPoints(ListNode head) {
-        ArrayList<Integer> arrli=new ArrayList<>();
-        int len=0;
-        int ans[]=new int[2];
-        Arrays.fill(ans,-1);
-        if(head==null || head.next==null||head.next.next==null)return ans;
-        ListNode travel=head.next;
-        ListNode prev=head;
-        // ListNode next=null;
-        int point =1;
-        while(travel.next!=null)
-        {
-            int curr=travel.val;
-            int previous=prev.val;
-            int next1=travel.next.val;
-            if(curr>previous && curr>next1)
-            {
-                arrli.add(point);
-            }
-            if(curr<previous && curr<next1){
-                arrli.add(point);
-            }
-            point++;
-            prev=travel;
-            travel=travel.next;
+        if(head.next.next==null) return new int[]{-1,-1};
+        int index=2;   // to see the index it is 2 because 1st node cant be critical point
+        ListNode prev=head;          // point to previous node of current node
+        ListNode curr=head.next;     // to traverse linked list
+        int min=Integer.MAX_VALUE;  //to store first critical point
+        int max=Integer.MIN_VALUE;  // to store last critical point
+        int min_diff=Integer.MAX_VALUE;//to store the minimum distance btw to critical point
+        int prevs=0;               // to store value of previous critical point
+        while(curr.next!=null){
+        if((curr.val>prev.val && curr.val>curr.next.val) || (curr.val<prev.val && curr.val<curr.next.val)){ // condition
+            max=Math.max(max,index); // finding last critical point
+            min=Math.min(min,index); // finding first critical point
+            if(prevs!=0)            //  if this is the second critical point then find its distance with the previous critical point
+                min_diff=Math.min(min_diff,index-prevs);
+            prevs=index;             // shift the previous critical point to the current one
         }
-        if(arrli.size()==0 || arrli.size()==1)return ans;
-        int min=Integer.MAX_VALUE;
-        for(int i = 0; i < arrli.size() - 1; i++){
-            min = Math.min(min, arrli.get(i + 1) - arrli.get(i));
+        prev=curr;               // update the previous pointer   
+        curr=curr.next;         // move to the next node
+        index++;               // increase the index  
         }
-        ans[0]=min;
-        ans[1]=arrli.get(arrli.size()-1)-arrli.get(0);
-        return ans;
-        
+       
+        if(min!=Integer.MAX_VALUE && max!=Integer.MIN_VALUE && min!=max){
+            return new int[]{min_diff,max-min};  // max distance is always between first and last critical point and minimum we have already maintained;
+        }
+        else{
+            return new int[]{-1,-1};
+        }
+
     }
 }
